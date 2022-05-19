fdca3a2dfeaadc9406a1e5f4060410656455ff78
JuanE <juanernestogranados@gmail.com>
2022-05-19 14:12:14 -0500

    Implementd count onClick

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button
+          id="inc"
+          onClick={() => this.setState({ count: this.state.count + 1 })}
+        >
+          Incrementa
+        </button>
       </div>
     );
   }
